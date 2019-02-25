b19b1e15b81b4cb07f1cc6928513ff593db0f0d3
Ana Flores <anafrs26@gmail.com>
2019-02-25 12:38:22 -0600

    Actualización a README.md. Ya cuenta con la función de seleccionar rango de años en data.js.

--- src/data.js
+++ src/data.js
@@ -3,18 +3,34 @@ const filterData = (country,indicatorNm,year) => {
   const countryIndicators = countryData.indicators
    for (let i= 0;  i < countryIndicators.length; i++) {
     const indicatorData= countryIndicators[i]
-    if (indicatorData.indicatorName == indicatorNm ) {
-     return indicatorData.data[year];
+    if (indicatorData.indicatorCode = indicatorNm ) {
+     const figure = indicatorData.data[year];
+
+     if (figure == "") {
+     	return "ND"
+     }
+
+     return figure;
     }
  
    };
  }
  
+const rangeFilterData = (country, indicatorNm, sinceYear, untilYear) => {
+  const yearsData = [];
+  for (let currentYear = sinceYear; currentYear <= untilYear; currentYear++){
+    yearsData [currentYear -sinceYear]= filterData(country, indicatorNm, currentYear);
+
+  }
+  return yearsData;
+
+
+};
+
+
  
  
  
  window.filterData = filterData 
-
-
-          
+ window.rangeFilterData = rangeFilterData
 
