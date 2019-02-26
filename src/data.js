9964be2c5f4fd5716acc3aef283444ed2733ad02
Ana Flores <anafrs26@gmail.com>
2019-02-26 11:13:01 -0600

    Borrar comentarios en data.js

--- src/data.js
+++ src/data.js
@@ -1,33 +1,3 @@
-//const filterData = (country,indicatorNm,year) => {
-  /*const countryData = WORLDBANK[country];
-  const countryIndicators = countryData.indicators;
-   for (let i= 0;  i < countryIndicators.length; i++) {
-    const indicatorData= countryIndicators[i];
-    if (indicatorData.indicatorCode = indicatorNm ) {
-     const figure = indicatorData.data[year];
-
-     if (figure == "") {
-     	return "ND";
-     }
-     return figure;
-    }
- 
-   };
- };
-const rangeFilterData = (country, indicatorNm, sinceYear, untilYear) => {;
-  const yearsData = [];
-  for (let currentYear = sinceYear; currentYear <= untilYear; currentYear++){
-    yearsData [currentYear -sinceYear]= filterData(country, indicatorNm, currentYear);
-
-  }
-  return yearsData;
-
-};
-
- window.filterData = filterData 
- window.rangeFilterData = rangeFilterData;
-window.WORLDBANK = worldbank.js; */
-
 const filterData = (country,indicatorNm,year) => {
   const countryData = WORLDBANK[country]
   const countryIndicators = countryData.indicators
@@ -39,8 +9,7 @@ const filterData = (country,indicatorNm,year) => {
      if (figure == "") {
          return `<p id="resultOk">${year} : "ND"</p>`
      }
-     //console.log(year);
-     //console.log(figure);
+     
  
      const resultOk = `<p id="resultOk">${year} : ${figure.toFixed(1)}</p>`;
      return resultOk;
@@ -56,7 +25,7 @@ const filterData = (country,indicatorNm,year) => {
     yearsData [currentYear -sinceYear]= filterData(country, indicatorNm, currentYear);
  
   }
-  //console.log(yearsData);
+
   return yearsData.join("");
  
  
